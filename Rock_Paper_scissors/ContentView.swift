175be84e37f45cadcfc7a19632a622a1bad63804
Pritom-Kumar-Paul <ppritom5@gmail.com>
2026-02-26 23:05:51 +0600

    Initial Commit

---
+++ Rock_Paper_scissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rock_Paper_scissors
+//
+//  Created by Pritom on 26/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock_Paper_scissors/Rock_Paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_scissorsApp.swift
+//  Rock_Paper_scissors
+//
+//  Created by Pritom on 26/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
